108f453b902f1adcf3dd2f92ef1fdbc4e5a50462
fisherbyu <fisherandrew777@gmail.com>
2024-01-27 19:12:28 -0700

    Revert "Merge pull request #17 from fisherbyu/AF-15"
    This reverts commit 4b265acc694172165564e9e52bf7bb694fbe5d5a, reversing
    changes made to 6b5c1287cdc3eb5ec34d38dcd858a91774713e4a.

--- public/js/link.js
+++ public/js/link.js
@@ -1,33 +1,8 @@
-let openings = []
-
-function postOpening(id, name, duration, lat, long, time) {
-  const newOpening = {
-    id: id,
-    name: name,
-    duration: duration,
-    lat: lat,
-    long: long,
-    time: time
+function getUser() {
+    let name = document.getElementsByClassName("welcomeUser")[0];
+    name.innerText += ` ${localStorage.getItem("userName")}`;
   }
-  openings.push(newOpening)
-}
+  
+  getUser();
 
-postOpening(1, "A", "3", 0, 0, "time");
-
-console.log(openings)
-
-function updateSpotList() {
-  let listArea = document.getElementById("displaySpots");
-  openings.forEach(function(opening) {
-    // Create a new paragraph element
-    const paragraph = document.createElement("p");
-
-    // Set the inner HTML of the paragraph with the opening details
-    paragraph.innerHTML = `${opening.name} is available for ${opening.duration} minutes at (${opening.lat}, ${opening.long}) at ${opening.time}.`;
-
-    // Append the paragraph to the list area
-    listArea.appendChild(paragraph);
-  });
-}
-
-updateSpotList()
+  
